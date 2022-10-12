26c2628d8911ce637e3ea816285f8d039b9d45a4
Adam Cox <adamcox.dev@gmail.com>
2022-10-12 16:54:02 -0600

    Question 2 part 3

--- starter/part-3/part_3.py
+++ starter/part-3/part_3.py
@@ -21,6 +21,27 @@ book_breakdown(my_book)
 
 # Code below
 
+def get_author(book):
+    author_str= f"{book['author']} wrote this book."
+    print(author_str)
+def get_title(book):
+    title_str= f"{book['title']} is the title of the book."
+    print(title_str)
+def get_year(book):
+    year_str= f"The book was written in {book['year']}"
+    print(year_str)
+def get_rating(book):
+    rating_str= f"{book['rating']} is the rating of this book."
+    print(rating_str)
+def get_pages(book):
+    pages_str= f"This book has {book['pages']} pages."
+    print(pages_str)
+    
+get_author(my_book)    
+get_title(my_book)    
+get_year(my_book)    
+get_rating(my_book)    
+get_pages(my_book) 
 
 # Finally, create at least three new functions that might be useful as we expand our home library app. Perhaps thing of a way you could accept additional arguments when the function is called? Also, imagine you have a list filled with dictionaries like above.
 
